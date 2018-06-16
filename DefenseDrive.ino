f13f1386219cb9bf58a19d7082ef546cd94b6b77
Allen Izquierdo <izquia1@unlv.nevada.edu>
2018-06-16 13:27:36 -0700

    Change drive configuration to HDrive

--- DefenseDrive.ino
+++ DefenseDrive.ino
@@ -2,7 +2,7 @@
 #include <PS2X_lib.h>
 #include <NetworkTable.h>
 #include <AStar32U4.h>
-#include <Ddrive.h>
+#include <HDrive.h>
 //#include <LSMHeadless.h>
 
 // Construct Drive Chassis
@@ -10,7 +10,7 @@ PololuG2 motorL = PololuG2(6, 9, 7, true);
 PololuG2 motorM = PololuG2(4, 10, 8, true);
 PololuG2 motorR = PololuG2(12, 5, 11, true);
 
-Ddrive chassis = Ddrive(motorL, motorR, motorM);
+HDrive chassis = HDrive(motorL, motorR, motorM);
 
 // Network & Contoller
 PS2X ps2x;
@@ -55,10 +55,10 @@ void loop()
 		bool L2 = ps2x.Button(PSB_L2);
 		bool R1 = ps2x.Button(PSB_R1);
 		bool R2 = ps2x.Button(PSB_R2);
-		bool Triangle = ps2x.Button(PSB_TRIANGLE);
-		bool Square = ps2x.Button(PSB_SQUARE);
-		bool Cross = ps2x.Button(PSB_CROSS);
-		bool Circle = ps2x.Button(PSB_CIRCLE);
+		bool Triangle = ps2x.ButtonPressed(PSB_TRIANGLE);
+		bool Square = ps2x.ButtonPressed(PSB_SQUARE);
+		bool Cross = ps2x.ButtonPressed(PSB_CROSS);
+		bool Circle = ps2x.ButtonPressed(PSB_CIRCLE);
 		bool PAD_Up = ps2x.Button(PSB_PAD_UP);
 		bool PAD_Down = ps2x.Button(PSB_PAD_DOWN);
 		bool R1_Pressed = ps2x.ButtonPressed(PSB_R1);
@@ -71,29 +71,24 @@ void loop()
 		bool L2_Released = ps2x.ButtonReleased(PSB_L2);
 		bool Select_Pressed = ps2x.ButtonPressed(PSB_SELECT);
 		bool Start_Pressed = ps2x.ButtonPressed(PSB_START);
-		
-		if(Select_Pressed)
-		{
-			armMotors = !armMotors;
-		}
-
+		// Reverse Motors Buttons
 		if(Square)
-		{
-		chassis.drive(180, 1, ps2x.JoyStick(PSS_RX));
-		}
+			chassis.reverseMotor(chassis.HDRIVE_MOTOR::LEFT, !chassis.getReverseMotorValue(chassis.HDRIVE_MOTOR::LEFT));
 		if(Triangle)
-		{
-		}
+			chassis.reverseMotor(chassis.HDRIVE_MOTOR::MIDDLE, !chassis.getReverseMotorValue(chassis.HDRIVE_MOTOR::MIDDLE));
 		if(Circle)
-		{
-		chassis.drive(0, 1, ps2x.JoyStick(PSS_RX));
-		}else{
-		Vec2 vec = Vec2(ps2x.JoyStick(PSS_LX), -ps2x.JoyStick(PSS_LY));
-		chassis.drive(Vec2::angle(vec), Vec2::magnitude(vec), ps2x.JoyStick(PSS_RX));
-
-		}
+			chassis.reverseMotor(chassis.HDRIVE_MOTOR::RIGHT, !chassis.getReverseMotorValue(chassis.HDRIVE_MOTOR::RIGHT));
+			
 
 		// Chassis Control
+		float strafeFactor = 0;
+		if(L1)
+			strafeFactor = 1;
+		if(R1)
+			strafeFactor = -1;
+		chassis.drive(ps2x.JoyStick(PSS_LY), -ps2x.JoyStick(PSS_RY), strafeFactor);
+		// Reset Button Flags
+		ps2x.read_gamepad(); // clear release&pressed flags.
 	}
 	PololuG2::iterate();
 	if (network.getLastPS2PacketTime() > 500 || !armMotors)
