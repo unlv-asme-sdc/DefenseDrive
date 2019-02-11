bb01a4fa08836a807b2f14bfcf2d0d30144cabb9
Allen Izquierdo <izquia1@unlv.nevada.edu>
2019-02-11 14:52:18 -0800

    Update latest pins

--- DefenseDrive.ino
+++ DefenseDrive.ino
@@ -10,6 +10,11 @@ PololuG2 motorL = PololuG2(6, 9, 7, true);// pins
 //PololuG2 motorL = PololuG2(12, 5, 11, true);//shield2
 PololuG2 motorR = PololuG2(4, 10, 8, true);//shield1
 
+// Drop Lift Subsystem Motors
+//PololuG2 motorDropL = PololuG2(13, 11, 12, true);
+//PololuG2 motorDropR = PololuG2(5, 3, 2, true);
+PololuG2 motorDropL = PololuG2(5, 3, 2, true);
+PololuG2 motorDropR = PololuG2(13, 11, 12, true);
 TankDrive chassis = TankDrive(motorL, motorR);
 
 // Network & Contoller
@@ -35,6 +40,7 @@ void setup()
 	myPacketSerial.setPacketHandler([](const uint8_t* buffer, size_t size) {
 		network.processPacketFromSender(myPacketSerial, buffer, size);
 	});
+	chassis.reverseLeftMotors(reverseLeft = !reverseLeft);
 
 	ps2x.config_gamepad();
 	ps2x.read_gamepad();
@@ -57,9 +63,12 @@ void loop()
 		bool L2 = ps2x.Button(PSB_L2);
 		bool R1 = ps2x.Button(PSB_R1);
 		bool R2 = ps2x.Button(PSB_R2);
-		bool Triangle = ps2x.ButtonPressed(PSB_TRIANGLE);
+		bool Triangle_Pressed= ps2x.ButtonPressed(PSB_TRIANGLE);
+		bool Triangle_Released= ps2x.ButtonReleased(PSB_TRIANGLE);
+		bool Cross_Pressed = ps2x.ButtonPressed(PSB_CROSS);
+		bool Cross_Released = ps2x.ButtonReleased(PSB_CROSS);
+
 		bool Square = ps2x.ButtonPressed(PSB_SQUARE);
-		bool Cross = ps2x.ButtonPressed(PSB_CROSS);
 		bool Circle = ps2x.ButtonPressed(PSB_CIRCLE);
 		bool PAD_Up = ps2x.Button(PSB_PAD_UP);
 		bool PAD_Down = ps2x.Button(PSB_PAD_DOWN);
@@ -74,10 +83,46 @@ void loop()
 		bool Select_Pressed = ps2x.ButtonPressed(PSB_SELECT);
 		bool Start_Pressed = ps2x.ButtonPressed(PSB_START);
 		// Reverse Motors Buttons
+			
 		if(Square)
 			chassis.reverseLeftMotors(reverseLeft = !reverseLeft);
-		if(Triangle)
+		if(Circle)
 			chassis.reverseRightMotors(reverseRight = !reverseRight);
+
+		// Drop Subsystem
+		if(R1_Pressed)
+			motorDropR.setPower(0.7);
+
+		if(R2_Pressed)
+			motorDropR.setPower(-0.7);
+
+		if(L1_Pressed)
+			motorDropL.setPower(-0.7);
+
+		if(L2_Pressed)
+			motorDropL.setPower(0.7);
+
+		if(Triangle_Pressed)
+		{
+			motorDropR.setPower(0.7);
+			motorDropL.setPower(-0.7);
+		}
+
+		if(Cross_Pressed)
+		{
+			motorDropR.setPower(-0.7);
+			motorDropL.setPower(0.7);
+		}
+
+		if(R1_Released || R2_Released || L1_Released || L2_Released || Cross_Released || Triangle_Released)
+		{
+			motorDropL.setPower(0);
+			motorDropR.setPower(0);
+		}
+
+
+
+			
 			
 
 		// Chassis Control
@@ -90,10 +135,12 @@ void loop()
 	{
 		digitalWrite(6, LOW);
 		digitalWrite(4, LOW);
-		digitalWrite(12, LOW);
+		digitalWrite(13, LOW);
+		digitalWrite(5, LOW);
 	} else {
 		digitalWrite(6, HIGH);
 		digitalWrite(4, HIGH);
-		digitalWrite(12, HIGH);
+		digitalWrite(13, HIGH);
+		digitalWrite(5, HIGH);
 	}
 }
